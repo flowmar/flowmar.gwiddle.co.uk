74e746436aad2a7a7b7d4cd2a7f312022726c56f
Flowmar <omar.imam@kineticvortex.com>
2017-08-06 15:08:19 -0500

    Add Back Button to Web Basics

--- httpdocs/js/index.js
+++ httpdocs/js/index.js
@@ -1,20 +1,18 @@
-function responsive() {
-    $(window)
-        .resize(function () {
-            var viewportWidth = $(window).width();
-            if (viewportWidth >= 768) {
-                $(".navbar-right").removeClass("navbar-nav");
-                $('.navbar').addClass("navbar-tablet");
-            }
-        });
-
-    $(window).resize(function () {
+$(window)
+    .resize(function () {
         var viewportWidth = $(window).width();
-        if (viewportWidth >= 1100) {
-            $("#flow").addClass("moveFlow mt-5");
+        if (viewportWidth >= 768) {
+            $(".navbar-right").removeClass("navbar-nav");
+            $('.navbar').addClass("navbar-tablet");
         }
     });
-}
+
+$(window).resize(function () {
+    var viewportWidth = $(window).width();
+    if (viewportWidth >= 1100) {
+        $("#flow").addClass("moveFlow mt-5");
+    }
+});
 
 function loadResponsive() {
     var viewportWidth = $(window).width();
