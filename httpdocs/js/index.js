f502c821d9b9556c9808205b740ebfdb128709d6
Flowmar <omar.imam@kineticvortex.com>
2017-08-06 15:07:44 -0500

    Fix navigation responsive issue

--- httpdocs/js/index.js
+++ httpdocs/js/index.js
@@ -1,14 +1,33 @@
-$(window).resize(function () {
+function responsive() {
+    $(window)
+        .resize(function () {
+            var viewportWidth = $(window).width();
+            if (viewportWidth >= 768) {
+                $(".navbar-right").removeClass("navbar-nav");
+                $('.navbar').addClass("navbar-tablet");
+            }
+        });
+
+    $(window).resize(function () {
+        var viewportWidth = $(window).width();
+        if (viewportWidth >= 1100) {
+            $("#flow").addClass("moveFlow mt-5");
+        }
+    });
+}
+
+function loadResponsive() {
     var viewportWidth = $(window).width();
+
     if (viewportWidth >= 768) {
         $(".navbar-right").removeClass("navbar-nav");
         $('.navbar').addClass("navbar-tablet");
     }
-});
-
-$(window).resize(function () {
-    var viewportWidth = $(window).width();
     if (viewportWidth >= 1100) {
         $("#flow").addClass("moveFlow mt-5");
     }
+};
+
+$(document).ready(function () {
+    loadResponsive();
 });
