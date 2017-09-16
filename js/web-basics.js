025d22040e4480184d96ddeafe201141101c7fc1
Flowmar <omar.imam@kineticvortex.com>
2017-09-16 12:33:14 -0500

    major style changes to all pages

--- js/web-basics.js
+++ js/web-basics.js
@@ -1,63 +1,64 @@
 $(document).ready(function() {
-    clickable();
+  $('a').css('cursor', 'hand');
+  clickable();
 });
 
 function clickable() {
-    /* Wireframing */
+  /* Wireframing */
 
+  $('#wireframing').on('click', function() {
+    $(this).addClass('active');
+    $('#button-area-1').html(
+      '<a href="https://flowmar.github.io/HW-Wireframe"><button class="btn btn-unique animated bounceIn" type=button title="Live Link">Live Link</button></a><a href= "https://github.com/flowmar/HW-Wireframe"> <button class="btn btn-indigo animated rollIn" type="button" title="Source Code">Source Code</button></a><a href="https://github.com/flowmar/HW-Wireframe/blob/master/README.md"><button class="btn-deep-orange btn animated fadeIn" type=button title="READ ME!">README</button></a>'
+    );
     $('#wireframing').on('click', function() {
-        $(this).addClass('active');
-        $('#button-area-1').html(
-            '<a href="https://flowmar.github.io/HW-Wireframe"><button class="btn btn-unique animated bounceIn" type=button title="Live Link">Live Link</button></a><a href= "https://github.com/flowmar/HW-Wireframe"> <button class="btn btn-indigo animated rollIn" type="button" title="Source Code">Source Code</button></a><a href="https://github.com/flowmar/HW-Wireframe/blob/master/README.md"><button class="btn-deep-orange btn animated fadeIn" type=button title="READ ME!">README</button></a>'
-        );
-        $('#wireframing').on('click', function() {
-            $(this).removeClass('active');
-            $('#button-area-1').empty();
-            clickable();
-        });
+      $(this).removeClass('active');
+      $('#button-area-1').empty();
+      clickable();
     });
+  });
 
-    /* Basic */
+  /* Basic */
 
+  $('#basic').on('click', function() {
+    $(this).addClass('active');
+    $('#button-area-2').html(
+      '<a href="https://flowmar.github.io/Basic-Portfolio"><button class="btn btn-unique animated bounceIn" type=button title="Live Link">Live Link</button></a><a href= "https://github.com/flowmar/Basic-Portfolio"> <button class="btn btn-indigo animated rollIn" type="button" title="Source Code">Source Code</button></a><a href="https://github.com/flowmar/Basic-Portfolio/blob/master/README.md"><button class="btn-deep-orange btn animated fadeIn" type=button title="READ ME!">README</button></a>'
+    );
     $('#basic').on('click', function() {
-        $(this).addClass('active');
-        $('#button-area-2').html(
-            '<a href="https://flowmar.github.io/Basic-Portfolio"><button class="btn btn-unique animated bounceIn" type=button title="Live Link">Live Link</button></a><a href= "https://github.com/flowmar/Basic-Portfolio"> <button class="btn btn-indigo animated rollIn" type="button" title="Source Code">Source Code</button></a><a href="https://github.com/flowmar/Basic-Portfolio/blob/master/README.md"><button class="btn-deep-orange btn animated fadeIn" type=button title="READ ME!">README</button></a>'
-        );
-        $('#basic').on('click', function() {
-            $(this).removeClass('active');
-            $('#button-area-2').empty();
-            clickable();
-        });
+      $(this).removeClass('active');
+      $('#button-area-2').empty();
+      clickable();
     });
+  });
 
-    /* Responsive */
+  /* Responsive */
 
+  $('#responsive').on('click', function() {
+    $(this).addClass('active');
+    $('#button-area-3').html(
+      '<a href="https://flowmar.github.io/Responsive-Portfolio"><button class="btn btn-unique animated bounceIn" type=button title="Live Link">Live Link</button></a><a href= "https://github.com/flowmar/Responsive-Portfolio"> <button class="btn btn-indigo animated rollIn" type="button" title="Source Code">Source Code</button></a><a href="https://github.com/flowmar/Responsive-Portfolio/blob/master/README.md"><button class="btn-deep-orange btn animated fadeIn" type=button title="READ ME!">README</button></a>'
+    );
     $('#responsive').on('click', function() {
-        $(this).addClass('active');
-        $('#button-area-3').html(
-            '<a href="https://flowmar.github.io/Responsive-Portfolio"><button class="btn btn-unique animated bounceIn" type=button title="Live Link">Live Link</button></a><a href= "https://github.com/flowmar/Responsive-Portfolio"> <button class="btn btn-indigo animated rollIn" type="button" title="Source Code">Source Code</button></a><a href="https://github.com/flowmar/Responsive-Portfolio/blob/master/README.md"><button class="btn-deep-orange btn animated fadeIn" type=button title="READ ME!">README</button></a>'
-        );
-        $('#responsive').on('click', function() {
-            $(this).removeClass('active');
-            $('#button-area-3').empty();
-            clickable();
-        });
+      $(this).removeClass('active');
+      $('#button-area-3').empty();
+      clickable();
     });
+  });
 
-    /* Bootstrap */
+  /* Bootstrap */
 
+  $('#bootstrap').on('click', function() {
+    $(this).addClass('active');
+    $('#button-area-4').html(
+      '<a href="https://flowmar.github.io/Bootstrap-Portfolio"><button class="btn btn-unique animated bounceIn" type=button title="Live Link">Live Link</button></a><a href= "https://github.com/flowmar/Bootstrap-Portfolio"> <button class="btn btn-indigo animated rollIn" type="button" title="Source Code">Source Code</button></a><a href="https://github.com/flowmar/Bootstrap-Portfolio/blob/master/README.md"><button class="btn-deep-orange btn animated fadeIn" type=button title="READ ME!">README</button></a>'
+    );
     $('#bootstrap').on('click', function() {
-        $(this).addClass('active');
-        $('#button-area-4').html(
-            '<a href="https://flowmar.github.io/Bootstrap-Portfolio"><button class="btn btn-unique animated bounceIn" type=button title="Live Link">Live Link</button></a><a href= "https://github.com/flowmar/Bootstrap-Portfolio"> <button class="btn btn-indigo animated rollIn" type="button" title="Source Code">Source Code</button></a><a href="https://github.com/flowmar/Bootstrap-Portfolio/blob/master/README.md"><button class="btn-deep-orange btn animated fadeIn" type=button title="READ ME!">README</button></a>'
-        );
-        $('#bootstrap').on('click', function() {
-            $(this).removeClass('active');
-            $('#button-area-4').empty();
-            clickable();
-        });
+      $(this).removeClass('active');
+      $('#button-area-4').empty();
+      clickable();
     });
+  });
 }
 //./2-basic-portfolio/basic-portfolio.html
 //./3-responsive-portfolio/responsive-portfolio.html
